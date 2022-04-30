e1b37db1780f6010605966a39579c01bd1040e92
Anna Beatriz <annab_oliver@hotmail.com>
2022-04-29 22:28:47 -0300

    exercicio iniciado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double nota1, nota2, nota3, nota4;
+
+Console.WriteLine("-- Média --");
+Console.WriteLine("Digite as suas notas abaixo.");
+
+Console.WriteLine("Nota 1:");
+nota1 = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Nota 2:");
+nota2 = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Nota 3:");
+nota3 = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Nota 4:");
+nota4 = Convert.ToDouble (Console.ReadLine());
+
+
+
+
+
+
+
+
